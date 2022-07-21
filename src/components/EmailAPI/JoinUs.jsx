8b97141100d3376ffa4003dd8380d95052115214
Priyanshu-Tyagi <102712899+Priyanshu-Tyagi@users.noreply.github.com>
2022-07-21 11:51:07 +0530

    fixed navbar Navlink issue

---
+++ src/components/EmailAPI/JoinUs.jsx
@@ -0,0 +1,10 @@
+
+
+export default function JoinUs(){
+    return (
+        <div>
+            <img src="https://drive.google.com/uc?export=view&id=1x3Qw_nTjDse95zmtTheOW1qvFSW6Uw3m" alt="" />
+            {/* https://drive.google.com/file/d/1x3Qw_nTjDse95zmtTheOW1qvFSW6Uw3m/view?usp=sharing */}
+        </div>
+    )
+}
