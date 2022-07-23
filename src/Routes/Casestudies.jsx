d9d969a1df059b2791349046d64c332d4c65f77b
Priyanshu Tyagi <102712899+Priyanshu-Tyagi@users.noreply.github.com>
2022-07-23 12:25:06 +0530

    Update Casestudies.jsx

--- src/Routes/Casestudies.jsx
+++ src/Routes/Casestudies.jsx
@@ -3,14 +3,13 @@ import Cases from "../components/CaseStudies/Cases"
 import Footer from "../components/Footer"
 import CreateFreeAccount from "../components/HomePage/CreateFreeAccount"
 import TrustedBy from "../components/HomePage/TrustedBy"
+import Navbar from "../components/Navbar";
 
 export default function CaseStudies(){
     return (
         <div>
+            <Navbar/>
             <CaseIntro/>
-            {/* <br/>
-            <br/>
-            <br/> */}
             <Cases/>
             <TrustedBy/>
             <CreateFreeAccount/>
