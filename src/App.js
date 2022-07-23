334862b6912623a7b66b5bdd3d6b15ad7a396be5
Priyanshu Tyagi <102712899+Priyanshu-Tyagi@users.noreply.github.com>
2022-07-23 12:23:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import Navbar from './components/Navbar';
 import AllRoutes from './Routes/AllRoutes';
 import * as React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
@@ -8,7 +7,6 @@ import { ChakraProvider } from '@chakra-ui/react';
 function App() {
   return (
     <ChakraProvider>
-      <Navbar />
       <AllRoutes/>
     </ChakraProvider>
   );
